4cff978569d8718ed1dd227d3acfea3960e1f29a
trick0846 <163693595+trick0846@users.noreply.github.com>
2024-09-17 20:30:34 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.py
@@ -6,7 +6,7 @@ n = int(sys.stdin.readline())
 # 수열을 입력받는다.
 arr = list(map(int,sys.stdin.readline().split()))
 
-# LDS를 담을 변수
+# 정답을 담을 변수
 answer = 1
 
 # 각 숫자별 최대 길이를 작성할 리스트 생성 / 수열의 크기와 리스트의 크기 동일
@@ -16,7 +16,7 @@ dp = [1] * n
 
 for i in range(1,n):
     for j in range(i):
-        # 해당 숫자가 비교하려는 숫자보다 작고, 해당 숫자까지의 부분수열의 길이보다 길어질 때
+        # 해당 숫자가 비교하려는 숫자보다 작고, [해당 숫자까지의 부분수열의 길이보다 길어질 때]
         if arr[i] < arr[j] and dp[i] < dp[j] + 1:
             # 비교하려는 숫자까지의 가장 긴 감소하는 부분 수열의 길이 + 1
             dp[i] = dp[j] + 1
