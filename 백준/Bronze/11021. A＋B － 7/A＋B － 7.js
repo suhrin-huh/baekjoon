0702c4d9e722a3cabe6575e571a52c3af033f035
황민채(suhrin-huh) <suhrin79@naver.com>
2025-01-03 20:49:57 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 9520 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -1,11 +1,17 @@
 const fs = require("fs");
-const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const path = process.platform == "linux" ? "/dev/stdin" : "./input.txt";
 
-const input = fs.readFileSync(path).toString().trim().split("\n");
-const tc = Number(input[0]); // Number of test cases
-const data = input.slice(1).map((line) => line.split(" ").map(Number)); // Parse test case data
+const input = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => el.split(" ").map(Number));
+
+const [TC, ...data] = input;
+const tc = TC.at(0);
+const answer = data.map((el) => el.at(0) + el.at(1));
 
 for (let i = 1; i <= tc; i++) {
-  const [a, b] = data[i - 1]; // Access each test case
-  console.log(`Case #${i}: ${a + b}`); // Output the result
+  console.log(`Case #${i}: ${answer.at(i - 1)}`);
 }
