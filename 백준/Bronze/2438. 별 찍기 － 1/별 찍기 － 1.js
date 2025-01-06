2535aa69c157f0e925f80e56476e02c9d190d3b2
황민채(suhrin-huh) <suhrin79@naver.com>
2025-01-06 22:45:55 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 100 ms, Memory: 9496 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -1,20 +1,10 @@
-const readline = require('readline');
+const fs = require("fs");
+const path = process.platform == "linux" ? "/dev/stdin" : "./input.txt";
 
-// 입력을 처리하기 위한 인터페이스 생성
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const input = fs.readFileSync(path).toString().trim();
+const num = Number(input);
+const word = "*";
 
-// 입력받기
-rl.question('', (input) => {
-  const N = parseInt(input); // 입력을 숫자로 변환
-
-  // N이 유효한 범위 내인지 확인
-    // 별 출력
-    for (let i = 1; i <= N; i++) {
-      console.log('*'.repeat(i)); // i번 반복해서 별 출력
-    }
-
-  rl.close(); // 입력 인터페이스 닫기
-});
+for (let i = 1; i <= num; i++) {
+  console.log(word.repeat(i));
+}
